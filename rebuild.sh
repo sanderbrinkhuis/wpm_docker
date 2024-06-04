85faec4c0e7585ed393cdd208ac57fc368951bc3
Sander Brinkhuis <sander.brinkhuis@inqdo.com>
2024-06-04 09:59:39 +0200

    use sag token in build

--- rebuild.sh
+++ rebuild.sh
@@ -1,4 +1,4 @@
 #bin\bash
 docker build -t is-wpm:10.15 -f DockerfileWPM .
 # no cache because there could be changes in other git repo's used by wpm
-docker build --no-cache -t is-run:10.15 -f DockerfileRUN .
+docker build --build-arg="SAG_TOKEN=${SAG_TOKEN}" --no-cache -t is-run:10.15 -f DockerfileRUN .
