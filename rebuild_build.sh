1f1f94a7cafe9ca006cbe4c82ced8325c2c059ef
Sander Brinkhuis <sander.brinkhuis@inqdo.com>
2024-06-06 09:56:35 +0200

    including delete rm and start

--- rebuild_build.sh
+++ rebuild_build.sh
@@ -2,12 +2,11 @@
 # login to sag repo
 # echo <password> | docker login --password-stdin --username sander-brinkhuis-inqdo-com sagcr.azurecr.io
 
+#echo <password> | docker login -u sander-brinkhuis-inqdo-com --password-stdin sagcr.azurecr.io
 cd /mnt/sag/repos/wpm_docker/
 aws s3 cp s3://dmg-shared-temp/wpm/customapp.properties .
 docker build -t iswpm:10.15 -f DockerfileWPM .
 # no cache because there could be changes in other git repo's used by wpm
 docker build --build-arg="SAG_TOKEN=${SAG_TOKEN}" --no-cache -t isrun:10.15 -f DockerfileRUN .
-
-
-
-# docker run -d --name is -p 5556:5555 -v /mnt/sag/repos/wpm_docker/customapp.properties:/opt/data/customapp.properties -e SAG_IS_CONFIG_PROPERTIES=/opt/data/customapp.properties is-run:10.15
+docker stop is && docker rm is
+docker run -d --name is -p 5556:5555 -v /mnt/sag/repos/wpm_docker/customapp.properties:/opt/data/customapp.properties -e SAG_IS_CONFIG_PROPERTIES=/opt/data/customapp.properties is-run:10.15
