18580bc7e50c8d7e524126876084dba355500d3f
Sander Brinkhuis <sander.brinkhuis@inqdo.com>
2024-06-11 08:55:51 +0200

    working ubi version

--- rebuild_mac.sh
+++ rebuild_mac.sh
@@ -11,7 +11,7 @@ docker context use rancher-desktop
 #docker build -t is-wpm:10.15 -f DockerfileWPM . --platform=linux/amd64
 docker build -t is-wpm:10.15 -f DockerfileWPM . --platform=linux/amd64 --build-arg TRUSTSTORE_PASSWORD=changeit --progress=plain
 # no cache because there could be changes in other git repo's used by wpm
-docker build --build-arg="SAG_TOKEN=${SAG_TOKEN}" --no-cache -t is-run:10.15 -f DockerfileRUN . --platform=linux/amd64
+docker build --build-arg="SAG_TOKEN=${SAG_TOKEN}" --no-cache -t is-run:10.15 -f DockerfileRUN . --platform=linux/amd64  --progress=plain
 
 # docker run -d --name is -p 5556:5555 -v /Users/lennertbrinkhuis/WmGits/wpm_docker/customapp.properties:/opt/data/customapp.properties -e SAG_IS_CONFIG_PROPERTIES=/opt/data/customapp.properties is-run:10.15
 # docker run -d --name is -p 5556:5555 is-run:10.15
